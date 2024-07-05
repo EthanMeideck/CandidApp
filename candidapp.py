d47b2cc19787554223de4d627e5af4b2a05a747d
NoRushJustPanda <ethan@meideck.net>
2024-07-05 12:43:21 +0200

    Structure projet avec commentaire

---
+++ candidapp.py
@@ -0,0 +1,14 @@
+from PySide2 import QtWidgets
+
+#Création classe
+
+#Récupérer entreprise avec une majuscule a chaque mot
+#Récupérer statut
+
+#Charger le fichier json
+
+#Ecrire dans un fichier json (dictionnaire ?)
+
+#Retirer entreprise avec le statut associé
+
+#Importer depuis un fichier word ou txt
