e21cf945ef53146bf07718ce452ecfa6735a53bf
NoRushJustPanda <ethan@meideck.net>
2024-07-05 20:16:34 +0200

    Add __init__ loading method and change to an empty data.json

--- candidapp.py
+++ candidapp.py
@@ -1,6 +1,19 @@
 from PySide2 import QtWidgets
+import os
+import json
 
-#Création classe
+CUR_DIR = os.path.dirname(__file__)
+DATA_FILE = os.path.join(CUR_DIR, "data", "data.json")
+
+class Candidapp:
+
+    def __init__(self, title, status):
+        self.title = title
+        self.status = status
+
+    def _get_society(self):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
 
 #Récupérer entreprise avec une majuscule a chaque mot
 #Récupérer statut
@@ -12,3 +25,7 @@ from PySide2 import QtWidgets
 #Retirer entreprise avec le statut associé
 
 #Importer depuis un fichier word ou txt
+
+if __name__ == "__main__":
+    c = Candidapp("t", "t")
+    c._get_society()
