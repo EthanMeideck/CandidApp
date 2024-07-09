7f26e86c45d930b366d389118007bb26f857d5c0
NoRushJustPanda <ethan@meideck.net>
2024-07-09 16:26:20 +0200

    Add Push button and list

--- app.py
+++ app.py
@@ -16,34 +16,56 @@ class App(QtWidgets.QWidget):
 
         self.text_layout = QtWidgets.QHBoxLayout()
         self.le_layout = QtWidgets.QHBoxLayout()
+        self.qpb_add_layout = QtWidgets.QVBoxLayout()
+        self.list_layout = QtWidgets.QHBoxLayout()
+        self.qpb_remove_layout = QtWidgets.QVBoxLayout()
 
-        #Label creation
+        #Labels
 
         self.text_society = QtWidgets.QLabel("Society name")
         self.text_status = QtWidgets.QLabel("Status")
 
-        #Adding label in the first horizontal layout
-
-        self.text_layout.addSpacerItem(QtWidgets.QSpacerItem(34, 0))
+        self.text_layout.addSpacerItem(QtWidgets.QSpacerItem(67, 0))
         self.text_layout.addWidget(self.text_society)
-        self.text_layout.addSpacerItem(QtWidgets.QSpacerItem(123, 0))
+        self.text_layout.addSpacerItem(QtWidgets.QSpacerItem(160, 0))
         self.text_layout.addWidget(self.text_status)
 
-        #Line edit creation
+        #Line edit 
 
         self.le_society = QtWidgets.QLineEdit()
         self.le_status = QtWidgets.QLineEdit()
-
-        #Adding line edit in the second horizontal layout
 
         self.le_layout.addWidget(self.le_society)
         self.le_layout.addSpacerItem(QtWidgets.QSpacerItem(30, 0))
         self.le_layout.addWidget(self.le_status)
 
+        #QPushButton to add items
+
+        self.qpb_add_item = QtWidgets.QPushButton("Add the society with the status")
+     
+        self.qpb_add_layout.addWidget(self.qpb_add_item)
+
+        #List with society and status
+
+        self.list_society = QtWidgets.QListView()
+        self.list_status = QtWidgets.QListView()
+
+        self.list_layout.addWidget(self.list_society)
+        self.list_layout.setSpacing(0)
+        self.list_layout.addWidget(self.list_status)
+
+        #QPushButton to remove items
+
+        self.qpb_remove_item = QtWidgets.QPushButton("Delete the society with the status")
+        self.qpb_remove_layout.addWidget(self.qpb_remove_item)
+
         #Adding secondary layout to the main layout
 
         self.main_layout.addLayout(self.text_layout)
         self.main_layout.addLayout(self.le_layout)
+        self.main_layout.addLayout(self.qpb_add_layout)
+        self.main_layout.addLayout(self.list_layout)
+        self.main_layout.addLayout(self.qpb_remove_layout)
 
         self.setLayout(self.main_layout)
 
